13643587cc8ed16016724edaac2eeafb9096e6ca
Zane Brzezinski <zanebrzezinski@gmail.com>
2016-03-01 18:51:42 -0500

    Create issues flux loop.  End of day commit.

--- lib/frontend/components/index.jsx
+++ lib/frontend/components/index.jsx
@@ -1,18 +1,36 @@
 var React = require('react');
 var marked = require('marked');
 var issuesJSON = require('./issues');
+var issuesUtil = require('../util/issues_util.js');
+var IssuesStore = require('../stores/issues_store');
 
 var IndexItem = require('./indexItem');
 
 var Index = React.createClass({
 
-  render: function() {
+  getInitialState: function() {
+    return ({issues: null});
+  },
 
-    var issues = issuesJSON.map(function(issue) {
-      return (
-        <IndexItem key={issue.id} issue={issue}/>
-      );
-    });
+  componentDidMount: function() {
+    this.token = IssuesStore.addListener(this._onChange);
+    issuesUtil.fetchIssues();
+  },
+
+  _onChange: function() {
+    this.setState({issues: IssuesStore.issues()});
+  },
+
+  render: function() {
+    var issues;
+    if (this.state.issues) {
+      issues = this.state.issues.map(function(issue) {
+        return (
+          <IndexItem key={issue.id} issue={issue}/>
+        );
+      });
+    }
+
 
     return(
       <ul className="issues">{issues}</ul>
