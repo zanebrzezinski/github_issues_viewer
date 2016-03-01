5be7d8afd27150f07a22026bc3a147727d299ecc
Zane Brzezinski <zanebrzezinski@gmail.com>
2016-03-01 18:04:59 -0500

    Refactor index items into new commponent IndexItem

---
+++ lib/frontend/components/indexItem.jsx
@@ -0,0 +1,50 @@
+var React = require('react');
+var marked = require('marked');
+var issuesJSON = require('./issues');
+
+var IndexItem = React.createClass ({
+  render: function () {
+    var issue = this.props.issue;
+    var labels = issue.labels.map(function(label) {
+      return (
+        <li key={label.name} className="label"
+        style={{backgroundColor: "#" + label.color}}
+        ><a href={label.url}>{label.name}</a></li>
+      );
+    });
+
+    var status;
+    if (issue.state === "open") {
+      status = <i className="fa fa-exclamation open-issue">Open</i>;
+    } else {
+      status = <i className="fa fa-check-circle-o closed-issue">Closed</i>;
+    }
+    var previewText = issue.body.slice(0,140);
+    var preview = {__html: previewText};
+
+    return(
+      <li>
+        <ul key={issue.id} className="issue group">
+          <div className="issue-info">
+            <div key="status" className="status">{status}</div>
+            <div key="id" className="id">{"#"+issue.id}</div>
+            <a href={"https://github.com/" + issue.user.login}>
+              <img key="avatar" className="avatar" src={issue.user.avatar_url}/>
+              <div key="username" className="username">
+                {"@" + issue.user.login}
+              </div>
+            </a>
+          </div>
+          <li key="title" className="title">{issue.title}</li>
+          <ul key="labels">{labels}</ul>
+          <li dangerouslySetInnerHTML={preview} className="preview"></li>
+          <li key="numComments" className="numComments">
+            <i className="fa fa-comment-o"></i> {issue.comments + " comments"}
+          </li>
+        </ul>
+      </li>
+    );
+  }
+});
+
+module.exports = IndexItem;
