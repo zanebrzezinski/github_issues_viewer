4be3b88afd24984b7649247bc74c0ef14195de2b
Zane Brzezinski <zanebrzezinski@gmail.com>
2016-03-01 14:37:08 -0500

    Finish adding necessary info to index list. Begin styling.

--- lib/frontend/components/index.jsx
+++ lib/frontend/components/index.jsx
@@ -9,16 +9,38 @@ var Index = React.createClass({
   render: function() {
 
     var issues = issuesJSON.map(function(issue) {
+      var labels = issue.labels.map(function(label) {
+        return (
+          <li key={label.name}><a href={label.url}>{label.name}</a></li>
+        );
+      });
+
+      var status;
+      if (issue.state === "open") {
+        status = <i className="fa fa-exclamation-circle"></i>;
+      } else {
+        status = <i className="fa fa-check-circle-o"></i>;
+      }
+
+      var preview = {__html: marked(issue.body.slice(0,140))};
+      var title = {__html: marked(issue.title)};
+
       return(
-        <li key={issue.id}>
-          <div key="username">{issue.user.login}</div>
-          <div key="issue">{issue.title}</div>
-        </li>
+        <ul key={issue.id} className="issue">
+          <li key="avatar" className="avatar"><img src={issue.user.avatar_url}/></li>
+          <li key="username">{issue.user.login}</li>
+          <li key="status">{status}</li>
+          <li key="id">{issue.id}</li>
+          <li key="title" dangerouslySetInnerHTML={title}></li>
+          <ul key="labels">{labels}</ul>
+          <li dangerouslySetInnerHTML={preview}></li>
+          <li key="numComments">{issue.comments}</li>
+        </ul>
       );
     });
 
     return(
-      <ul>{issues}</ul>
+      <ul className="issues">{issues}</ul>
     );
   }
 });
